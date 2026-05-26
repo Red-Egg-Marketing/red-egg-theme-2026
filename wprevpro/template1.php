53157ee7cc84451a784d175b594fdeddc56a577f
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2026-05-26 14:48:59 -0600

    updates to review template

---
+++ wprevpro/template1.php
@@ -0,0 +1,13 @@
+<div class="<?php echo $tempmasonrydiv; ?> outerrevdiv">
+			<div class="indrevdiv wprevpro_t1_DIV_2 wprev_preview_bg1_T<?php echo $currentform[0]->style; ?><?php if($iswidget){echo "_widget";} ?> wprev_preview_bradius_T<?php echo $currentform[0]->style; ?><?php if($iswidget){echo "_widget";} ?>">
+				<div class="indrevtxt wprevpro_t1_P_3 wprev_preview_tcolor1_T<?php echo $currentform[0]->style; ?><?php if($iswidget){echo "_widget";} ?>"><?php echo $rdmorescroll[0]; ?>
+					<span class="wprevpro_star_imgs_T<?php echo $currentform[0]->style; ?><?php if($iswidget){echo "_widget";} ?>"><?php echo $starhtml; ?><?php echo $verifiedstarhtmlarray[0]; ?></span><?php echo $title; ?><?php echo $reviewtext; ?><?php echo $verifiedstarhtmlarray[1]; ?><?php echo $rdmorescroll[1]; ?><?php echo $ownerresponse; ?>
+				</div>
+				<?php echo $media; ?>
+				<?php echo $miscpichtml; ?>
+				<?php echo $sourcepagenamehtml; ?>
+				<?php echo $logo; ?>
+				<?php echo $userpichtml; ?> <?php if($iswidget){echo "_widget";} ?><div class="wprevpro_t1_rname wprevname" ><?php echo $tempreviewername; ?></div><?php echo $companyhtml;?><div class="wprev_showdate_T<?php echo $currentform[0]->style; ?><?php if($iswidget){echo "_widget";} ?> wprevdate"><?php echo $datestring; ?></div>
+			</div>
+		<?php echo $readmorepopup; ?>	
+</div>
