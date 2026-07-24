2ba5d52037a8fa28da581ae431de58b4283cdef3
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2026-07-24 15:44:11 -0600

    added image sizes

--- wp-content/themes/red-egg-theme-2026/inc/media.php
+++ wp-content/themes/red-egg-theme-2026/inc/media.php
@@ -13,6 +13,8 @@ add_image_size('hero-landscape', 864, 300, array('center', 'center'), true);
 add_image_size('medium-large', 700, 700, array('center', 'center'), true);
 add_image_size('medium-landscape', 920, 460, array('center', 'center'), true);
 add_image_size('medium-small', 350, 350, array('center', 'center'), true);
+add_image_size('desktop-image', 1000, 562, array('center', 'center'), true);
+add_image_size('mobile-image', 414, 878, array('center', 'center'), true);
 
 add_filter( 'image_size_names_choose', 'emulate_custom_sizes' );
  
@@ -28,5 +30,7 @@ function emulate_custom_sizes( $sizes ) {
         'image-text-block' => ('Image & Text Block'),
         'image-text-block-small' => ('Image & Text Block Small'),
         'medium-large' => ('Medium/Large Image'),
+        'desktop-image' => ('Desktop Screen Image'),
+        'mobile-image' => ('Mobile Screen Image'),
     ) );
 }
